99683dc8a77ed6dbf906db38359666bbe6d120c4
Игорь <igorkhvorost@gmail.com>
2022-04-04 16:41:05 +0300

    index.js and css

--- index.js
+++ index.js
@@ -8,9 +8,9 @@ app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, '/index.html'));
   });
 
-//   app.get('/', (req, res) =>{
-//     res.sendFile(path.join(__dirname, 'assets/css/styless.css'));
-// });
+  app.get('/css', (req, res) =>{
+    res.sendFile(path.join(__dirname, 'assets/css/styless.css'));
+});
 
 app.use('/css', express.static(path.join(__dirname, 'assets/css')));
   
